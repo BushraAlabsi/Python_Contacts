04027a94bde0ff312b309e1d4b78651f008c4ed5
BushraAlabsi <boushra-93@live.com>
2018-08-08 14:42:22 +0300

    connecting the server

--- part1-Basic Python Server/requestHandler.py
+++ part1-Basic Python Server/requestHandler.py
@@ -7,11 +7,16 @@ class myHandler(BaseHTTPRequestHandler):
 	#This will handle GET requests
 	def do_GET(self):
 		# Add the outgoing status here
-		
+		print("Goodbye, World!")
 		# You will need to change this if you are sending something
   		# other than plain text, like JSON or HTML.
+  		self.send_response(200)
 		self.send_header('Content-type','text/html')
 		self.end_headers()
+		self.wfile.write("<html><head><title>Python Sprint</title></head>")
+		self.wfile.write("<body><p>Hello World</p>")
+		self.wfile.write("</body></html>")
+		
 
 		# Send the "Hello World" message here 
 		
